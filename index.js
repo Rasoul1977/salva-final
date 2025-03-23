42986dd94abe657824907cc3ec6cb89f2c32d2e4
Rasoul1977 <58551452+Rasoul1977@users.noreply.github.com>
2025-03-23 13:10:31 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Tahoma' }}>
+      <h1>رزرو اقامتگاه سلوا</h1>
+      <p>به سامانه رزرو آنلاین خوش آمدید 🌿</p>
+    </div>
+  );
+}
